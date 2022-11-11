33a34400e8ccb8c4d6062050551a58dd4464f7e9
josepmartorell <martorelljosep>
2022-11-11 16:22:14 +0100

    interface bug fixed

--- src/main/java/com/crud/h2/service/IFabricanteService.java
+++ src/main/java/com/crud/h2/service/IFabricanteService.java
@@ -9,11 +9,11 @@ public interface IFabricanteService {
 	//Metodos del CRUD
 		public List<Fabricante> listarFabricantes(); //Listar All 
 		
-		public Fabricante guardarFabricante(Fabricante video);	//Guarda un fabricante CREATE
+		public Fabricante guardarFabricante(Fabricante fabricante);	//Guarda un fabricante CREATE
 		
 		public Fabricante fabricanteXID(Long id); //Leer datos de un fabricante READ
 		
-		public Fabricante actualizarFabricante(Fabricante video); //Actualiza datos del fabricante UPDATE
+		public Fabricante actualizarFabricante(Fabricante fabricante); //Actualiza datos del fabricante UPDATE
 		
 		public void eliminarFabricante(Long id);// Elimina el fabricante DELETE
 		
