a9d530e5f045af1c37be3860fdcc2c741e7a5ab9
josepmartorell <martorelljosep>
2022-11-12 17:02:42 +0100

    inserts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO fabricante (nombre, fecha) VALUES ('MSI', PARSEDATETIME('12/03/2013','dd/MM/yyyy'));
+INSERT INTO fabricante (nombre, fecha) VALUES ('HP', PARSEDATETIME('12/03/2013','dd/MM/yyyy'));
+INSERT INTO fabricante (nombre, fecha) VALUES ('ASUS', PARSEDATETIME('12/03/2013','dd/MM/yyyy'));
+INSERT INTO fabricante (nombre, fecha) VALUES ('DELL', PARSEDATETIME('12/03/2013','dd/MM/yyyy'));
